544a2aa3cda3397b60549d5c0b00384a1d1cf4db
HodRZ <hodaifa.radwan@gmail.com>
2022-09-14 12:43:31 +0300

    Fix state management#2

--- src/components/post/PostsCard.jsx
+++ src/components/post/PostsCard.jsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { AiFillDelete } from 'react-icons/ai'
 function PostsCard(props) {
     const [comment, setComment] = useState(false)
-    const [post, setPost] = useState(props.post)
+    const [post, setPost] = useState()
+    const [showPost, setShowPost] = useState(true)
 
     const getPost = async () => {
         const id = props.post.id
@@ -34,37 +35,40 @@ function PostsCard(props) {
     const deletePost = async (e) => {
         e.preventDefault()
         const id = e.target.id
-        console.log(id)
         await axios.delete(`${process.env.REACT_APP_PORT}/post/${id}`)
-        getPost()
+        setShowPost(false)
     }
     useEffect(() => {
         getPost()
     },)
     return (
-        <div key={post.id} className=' bg-slate-200 border-2 flex flex-col border-slate-700 rounded-md h-fit '>
-            <div className='flex justify-between'>
-                <h2 className='text-center text-2xl mx-3 my-5'>{post.title}</h2>
-                <form onSubmit={deletePost} id={post.id} className='mt-3'>
-                    <button className='text-xl'><AiFillDelete className='h-6 w-fit border-2 m-2 rounded-full bg-slate-500 text-white border-white hover:bg-white hover:text-slate-500' /></button>
-                </form>
-            </div>
-            <p className='mx-5 my-8'>{post.content}</p>
-            <div className='bg-white flex flex-col gap-3 my-2'>
-                {post.comments &&
-                    post.comments.map((comment) => {
-                        return <div className='flex justify-between'>
-                            <p className='px-5 border-t-2'>{comment.content}</p>
-                            <button className='mx-2 text-sm border rounded-xl hover:bg-black hover:text-white border-black' id={comment.id} onClick={deleteComment} >delete</button>
-                        </div>
-                    }
-                    )}
-            </div>
-            <form className='flex flex-col my-3 place-items-center text-center' id={post.id} onSubmit={addComment}>
-                <input type="text" name='comment' placeholder='comment' className='w-full border border-black rounded-lg bg-slate-300 my-4' />
-                <button className='border-b-2 border-black rounded-xl w-16'>comment</button>
-            </form>
-        </div>
+        <>
+            {showPost &&
+                <div key={post?.id} className=' bg-slate-200 border-2 flex flex-col border-slate-700 rounded-md h-fit '>
+                    <div className='flex justify-between'>
+                        <h2 className='text-center text-2xl mx-3 my-5'>{post?.title}</h2>
+                        <form onSubmit={deletePost} id={post?.id} className='mt-3'>
+                            <button className='text-xl'><AiFillDelete className='h-6 w-fit border-2 m-2 rounded-full bg-slate-500 text-white border-white hover:bg-white hover:text-slate-500' /></button>
+                        </form>
+                    </div>
+                    <p className='mx-5 my-8'>{post?.content}</p>
+                    <div className='bg-white flex flex-col gap-3 my-2'>
+                        {post?.comments &&
+                            post.comments.map((comment) => {
+                                return <div className='flex justify-between'>
+                                    <p className='px-5 border-t-2'>{comment.content}</p>
+                                    <button className='mx-2 text-sm border rounded-xl hover:bg-black hover:text-white border-black' id={comment.id} onClick={deleteComment} >delete</button>
+                                </div>
+                            }
+                            )}
+                    </div>
+                    <form className='flex flex-col my-3 place-items-center text-center' id={post?.id} onSubmit={addComment}>
+                        <input type="text" name='comment' placeholder='comment' className='w-full border border-black rounded-lg bg-slate-300 my-4' />
+                        <button className='border-b-2 border-black rounded-xl w-16'>comment</button>
+                    </form>
+                </div>
+            }
+        </>
     );
 }
 
