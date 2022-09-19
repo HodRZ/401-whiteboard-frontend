5fb8f4cf8d7165f0cc8a57ecd8d28406847ce6d9
HodRZ <hodaifa.radwan@gmail.com>
2022-09-19 13:46:40 +0300

    Add basic authentication

--- src/components/main/Hero.jsx
+++ src/components/main/Hero.jsx
@@ -4,7 +4,7 @@ function Hero(props) {
     return (
         <div className='flex flex-col justify-center h-[25vh] pt-8 text-center'>
             <div className=' font-mono'>
-                <h2 className='text-5xl'>Got Jokes {props.user?.name}?</h2>
+                <h2 className='text-5xl'>Got Jokes?</h2>
             </div>
         </div>
     );
